9456a5635112f36b24c4c2610626d4ee83e96d23
KlausPopp <klauspopp@gmx.de>
2023-02-04 16:11:14 +0100

    WIP

---
+++ src/unix/logging.c
@@ -0,0 +1,13 @@
+#include <bits/time.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+uint32_t io4e_log_timestamp(void)
+{
+    struct timespec spec;
+    if (clock_gettime(CLOCK_MONOTONIC, &spec) == -1) {
+        return 0;
+    }
+    return spec.tv_sec * 1000 + spec.tv_nsec / 1e6;
+}
